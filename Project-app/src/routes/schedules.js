191c5fa9814da0f3b48b055178ba0304ebb4db34
Henrique Ferreira Da Silva <hefesi93@gmail.com>
2025-10-06 15:47:49 -0300

    comite rapido

--- Project-app/src/routes/schedules.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scheduleController = require('../controllers/scheduleController');
-
-// Use os nomes corretos das funções exportadas:
-router.post('/', scheduleController.criarHorario);
-router.get('/:id_usuario', scheduleController.listarHorarios);
-
-module.exports = router;
--- Project-app/src/routes/simuladoRoutes.js
+++ Project-app/src/routes/simuladoRoutes.js
@@ -6,17 +6,9 @@ const validationHandler = require("../validador/validationHandler");
 
 // Listar todos
 router.get("/", simuladoController.getAll);
-
-// Buscar por ID
 router.get("/:id", simuladoController.getById);
-
-// Criar com validação
 router.post("/", validateSimulado, validationHandler,simuladoController.create);
-
-// Atualizar com validação
 router.put("/:id",  validateSimulado, validationHandler,simuladoController.update);
-
-// Deletar
 router.delete("/:id", simuladoController.remove);
 
 module.exports = router;
